8255ebdab29a76c455122849a9333eaa365cffbb
ClauMpFreitas <claudinhampfreitas@gmail.com>
2023-12-13 13:11:07 -0300

    Atualização da second page

--- src/pages/secondpage.js
+++ src/pages/secondpage.js
@@ -1,13 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import {
-  Anchor,
-  grommet,
-  Grommet,
-  Page,
-  PageContent,
-  PageHeader,
-} from "grommet";
+import { grommet, Grommet, Page, PageContent, PageHeader } from "grommet";
 import { FormNext } from "grommet-icons";
 
 const SecondPage = () => (
@@ -19,15 +12,18 @@ const SecondPage = () => (
           description={`Next.js automatically handles the route based 
           on your file structure.`}
         />
-        {/* Need to pass href:
-         * https://github.com/zeit/next.js/#forcing-the-link-to-expose-href-to-its-child */}
-        <Link href="/" passHref>
-          <Anchor
-            alignSelf="start"
-            label="Back to Home page"
-            icon={<FormNext />}
-            reverse
-          />
+        <Link href="/">
+          <a
+            style={{
+              display: "flex",
+              alignItems: "center",
+              textDecoration: "none",
+              color: "inherit",
+            }}
+          >
+            <FormNext />
+            <span style={{ marginLeft: "8px" }}>Back to Home page</span>
+          </a>
         </Link>
       </PageContent>
     </Page>
