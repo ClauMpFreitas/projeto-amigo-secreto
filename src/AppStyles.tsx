ebe0860251ad27d509dec1fab758d05d0f65c4ef
Stella <stellamcruzr@gmail.com>
2023-12-20 14:58:26 -0300

    change to vite, antd create base of home page

---
+++ src/AppStyles.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+
+export const ScreenSC = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: #eeeeee;
+  overflow-x: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
