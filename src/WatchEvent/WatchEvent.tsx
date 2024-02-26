c1e88f8053de24d043bc84d3cef09fd9b4f07d62
ClauMpFreitas <claudinhampfreitas@gmail.com>
2024-02-26 14:32:26 -0300

    Inclusão da branch Login de participantes

---
+++ src/WatchEvent/WatchEvent.tsx
@@ -0,0 +1,82 @@
+import { Button, Card, Col, Row, Typography } from "antd";
+import { GiftAndPersonsSvg } from "../assets/Svgs/GiftAndPersonsSvg";
+import { useAppContext } from '../AppContext';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export function WatchEvent() {
+  const { eventInfo, participants } = useAppContext();
+  const [generatedLink, setGeneratedLink] = useState<string | null>(null);
+  const navigate = useNavigate();
+
+  const generateEventLink = () => {
+    const eventName = eventInfo.nomeevento || '';
+    const eventLink = `https://amigoalmoco.com/${eventName.replace(/\s/g, '-')}`;
+    return eventLink;
+  };
+
+  const handleGenerateLink = () => {
+    const link = generateEventLink();
+    setGeneratedLink(link);
+  };
+
+  const handleLinkClick = () => {
+    navigate("/cadastroparticipants");
+  };
+
+  return (
+    <>
+      <Row gutter={24}>
+        <Col span={12}>
+          <div style={{ marginLeft: '20px' }}>
+            <Typography.Title level={1}>Amigo Almoço</Typography.Title>
+            <Typography.Text>Bem-vindo ao nosso aplicativo para gerenciar amigo almoço</Typography.Text>
+            <GiftAndPersonsSvg/>
+          </div>
+        </Col>
+        <Col span={12}>
+          <Card style={{ width: 400, height: 450 }}>
+            <div style={{ marginBottom: '5px' }}>
+              <Typography.Title level={2} style={{ marginTop: '3px' }}>Detalhes do Evento</Typography.Title>
+              <Typography.Text strong>Nome do Evento: </Typography.Text>
+              <Typography.Text>{eventInfo.nomeevento}</Typography.Text>
+            </div>
+            <div style={{ marginBottom: '5px' }}>
+              <Typography.Text strong>Descrição: </Typography.Text>
+              <Typography.Text>{eventInfo.descricao}</Typography.Text>
+            </div>
+            <div style={{ marginBottom: '5px' }}>
+              <Typography.Text strong>Data do Sorteio: </Typography.Text>
+              <Typography.Text>{eventInfo.datasorteio?.format('DD/MM/YYYY')}</Typography.Text>
+            </div>
+            <div style={{ marginBottom: '5px' }}>
+              <Typography.Text strong>Data do Encontro: </Typography.Text>
+              <Typography.Text>{eventInfo.dataencontro?.format('DD/MM/YYYY')}</Typography.Text>
+            </div>
+            <Typography.Title level={3}>Lista de Participantes</Typography.Title>
+            <Row gutter={16}>
+              {participants.map((participant, index) => (
+                <Col span={6} key={index}>
+                  <Typography.Text>{participant}</Typography.Text>
+                </Col>
+              ))}
+            </Row>
+            <Button type="primary" onClick={handleGenerateLink} style={{ marginTop: '20px' }}>
+                Gerar Link para o Evento
+            </Button>
+            <div style={{ marginTop: '8px' }}>
+              <Typography.Text strong>Link Gerado: </Typography.Text>
+              {generatedLink ? (
+                <a href="#" onClick={handleLinkClick}>
+                  {generatedLink}
+                </a>
+              ) : (
+                <Typography.Text>Nenhum link gerado ainda</Typography.Text>
+              )}
+            </div>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+}
